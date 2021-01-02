72ee349a9890568f65e7f0d224cc38cfd7dbe3a0
YuhangJi <52953718+YuhangJi@users.noreply.github.com>
2021-01-02 09:41:06 +0800

    Add files via upload

--- BUCEALoginner/bucea_loginner.py
+++ BUCEALoginner/bucea_loginner.py
@@ -1,211 +1,229 @@
-# -*- coding: utf-8 -*-
+import re
 import time
-import random
 import argparse
-import urllib.error
-import urllib.parse
-import urllib.request
-import http.cookiejar
+import requests
+from hmac import new
+from hashlib import md5, sha1
 
 
-class BuceaLoginner:
-    """
-    2020年4月18日
-    """
+class BuceaNet(object):
 
-    url_dict = {"get_url": "http://10.1.1.131:903/srun_portal_pc.php?ac_id=1&",
-                "post_url": "http://10.1.1.131:903/srun_portal_pc.php?ac_id=1&url=www.msftconnecttest.com"}
-    ua_list = [
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/78.0.3904.108 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/79.0.3945.88 Safari/537.36 ",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363"]
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.sess = requests.session()
+        self.header = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                          "Chrome/86.0.4240.111 Safari/537.36 "
+        }
+        self.CHALLENGE_API = "http://10.1.1.131/cgi-bin/get_challenge"
+        self.SRUN_PORTAL_API = "http://10.1.1.131/cgi-bin/srun_portal"
+        self.ROOT_URL = "http://10.1.1.131"
 
-    cj = http.cookiejar.CookieJar()
-    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
+        self.ip = self.__get_ip()
+        self.token = self.__get_token()
+        self.ac_id = '1'
+        self.enc = "srun_bx1"
+        self.n = "200"
+        self.type = "1"
 
-    def __init__(self, user_name=None, pass_word=None,
-                 start_time=1, end_time=5):
+        self.info = self.__get_info()
+        self.hmd5 = self.__get_md5()
+        self.chksum = self.__get_chksum()
 
-        self.__ps = {"user_name": user_name,  # ps <=> parameters dictionary
-                     "pass_word": pass_word,
-                     "start_time": start_time,
-                     "end_time": end_time}
-
-    @staticmethod
-    def _get_time():
-        return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-
-    @staticmethod
-    def _get_content_length(data):
-        length = len(data.keys()) * 2 - 1
-        total = ''.join(list(data.keys()) + list(data.values()))
-        length += len(total)
-        return length
-
-    def _get_ua(self):
-        random.shuffle(self.ua_list)
-        return self.ua_list[0]
-
-    def down_time(self):
-        hour = int(self._get_time().split(" ")[-1].split(":")[0])
-        if self.__ps["start_time"] <= hour < self.__ps["end_time"]:
-            return True
-        else:
-            return False
-
-    def bucea_login(self):
-        state_code = {200: "Success to connect!", 400: "Connection has been failed!", 0: "unknown"}
-        ua = self._get_ua()  # user-agent
-        get_headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
-                      'application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Connection': 'keep-alive',
-            'Host': '10.1.1.131:903',
-            'Referer': 'http://10.1.1.131/index_1.html',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': ua}
-
-        try:
-            req = urllib.request.Request(url=self.url_dict["get_url"],
-                                         headers=get_headers)
-            self.opener.open(req)
-        except TimeoutError:
-            print(self._get_time(), "Program raised a TimeoutError with the long waiting time.")
-        except urllib.error.HTTPError as e:
-            print(self._get_time(), "HTTPError:{}".format(e.code))
-        except urllib.error.URLError as e:
-            print(self._get_time(), e.reason)
-
-        data_dict = {
+        self.srun_portal_params = {
+            'callback': 'jQuery11240645308969735664_' + str(int(time.time() * 1000)),
             'action': 'login',
-            'ac_id': '1',
-            'user_ip': '',
-            'nas_ip': '',
-            'user_mac': '',
-            'url': '',
-            'username': self.__ps["user_name"],
-            'password': self.__ps["pass_word"]}
+            'username': self.username,
+            'password': '{MD5}' + self.hmd5,
+            'ac_id': self.ac_id,
+            'ip': self.ip,
+            'chksum': self.chksum,
+            'info': self.info,
+            'n': self.n,
+            'type': self.type,
+            'os': 'windows+10',
+            'name': 'windows',
+            'double_stack': '0',
+            '_': int(time.time() * 1000)
+        }
 
-        content_length = str(self._get_content_length(data_dict))  # content_length
+    def login(self):
+        srun_portal_res = self.sess.post(self.SRUN_PORTAL_API,data=self.srun_portal_params,headers=self.header)
 
-        post_headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,'
-                      'application/signed-exchange;v=b3;q=0.9',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Content-Length': content_length,
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Host': '10.1.1.131:903',
-            'Origin': 'http://10.1.1.131:903',
-            'Referer': 'http://10.1.1.131:903/srun_portal_pc.php?ac_id=1&url=www.msftconnecttest.com',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': ua}
-        form_data = urllib.parse.urlencode(data_dict).encode(encoding='utf-8')  # encode post data
-        code = 0
+        if re.search("E0000", srun_portal_res.text):
+            print("Login Success.")
+        elif re.search("ip_already_online_error", srun_portal_res.text):
+            print("already online, client_ip is: {}".format(self.ip))
+        else:
+            print("Login Failed.")
 
-        try:
-            request = urllib.request.Request(url=self.url_dict["post_url"],
-                                             data=form_data,
-                                             headers=post_headers)
-            response = self.opener.open(request)
-            if response.getcode() == 200:
-                code = response.getcode()
-        except TimeoutError:
-            print(self._get_time(), "TimeoutError")
-            code = 400
-        except urllib.error.HTTPError as e:
-            print(self._get_time(), "HTTPError:{}".format(e.code))
-            code = 400
-        except urllib.error.URLError as e:
-            print(self._get_time(), e.reason)
-            code = 400
-        finally:
-            print(self._get_time(), state_code[code])
-        return code
+    def __get_ip(self):
+        init_res = self.sess.get(self.ROOT_URL, headers=self.header)
+        return re.search('ip(\s*):(\s*)"(.*)"', init_res.text).group(3)
+
+    def __get_token(self):
+        get_challenge_params = {
+            "callback": "jQuery112404953340710317169_" + str(int(time.time() * 1000)),
+            "username": self.username,
+            "ip": self.ip,
+            "_": int(time.time() * 1000),
+        }
+        get_challenge_res = requests.get(
+            self.CHALLENGE_API, params=get_challenge_params, headers=self.header
+        )
+        return re.search('"challenge":"(.*?)"', get_challenge_res.text).group(1)
+
+    def __get_info(self):
+        info_temp = {
+            "username": self.username,
+            "password": self.password,
+            "ip": self.ip,
+            "acid": self.ac_id,
+            "enc_ver": self.enc,
+        }
+        info = re.sub("'", '"', str(info_temp))
+        info = re.sub(" ", "", info)
+        info = "{SRBX1}" + self.__get_base64(self.__get_xencode(info, self.token))
+        return info
+
+    def __get_md5(self):
+        return new(self.token.encode(), self.password.encode(), md5).hexdigest()
+
+    def __get_chksum(self):
+        chkstr = self.token + self.username
+        chkstr += self.token + self.hmd5
+        chkstr += self.token + self.ac_id
+        chkstr += self.token + self.ip
+        chkstr += self.token + self.n
+        chkstr += self.token + self.type
+        chkstr += self.token + self.info
+        return sha1(chkstr.encode()).hexdigest()
+
+    @staticmethod
+    def __get_base64(s):
+        _PADCHAR = "="
+        _ALPHA = "LVoJPiCN2R8G90yg+hmFHuacZ1OWMnrsSTXkYpUq/3dlbfKwv6xztjI7DeBE45QA"
+        i = 0
+        b10 = 0
+        x = []
+        imax = len(s) - len(s) % 3
+
+        def get_byte(s_,i_):
+            return ord(s_[i_])
+
+        if len(s) == 0:
+            return s
+        for i in range(0, imax, 3):
+            b10 = (
+                    (get_byte(s, i) << 16) | (get_byte(s, i + 1) << 8) | get_byte(s, i + 2)
+            )
+            x.append(_ALPHA[(b10 >> 18)])
+            x.append(_ALPHA[((b10 >> 12) & 63)])
+            x.append(_ALPHA[((b10 >> 6) & 63)])
+            x.append(_ALPHA[(b10 & 63)])
+        i = imax
+        if len(s) - imax == 1:
+            b10 = get_byte(s, i) << 16
+            x.append(
+                _ALPHA[(b10 >> 18)] + _ALPHA[((b10 >> 12) & 63)] + _PADCHAR + _PADCHAR
+            )
+        elif len(s) - imax == 2:
+            b10 = (get_byte(s, i) << 16) | (get_byte(s, i + 1) << 8)
+            x.append(
+                _ALPHA[(b10 >> 18)]
+                + _ALPHA[((b10 >> 12) & 63)]
+                + _ALPHA[((b10 >> 6) & 63)]
+                + _PADCHAR
+            )
+        return "".join(x)
+
+    @staticmethod
+    def __get_xencode(msg, key):
+        from math import floor
+        if msg == "":
+            return ""
+
+        def ordat(msg, idx):
+            if len(msg) > idx:
+                return ord(msg[idx])
+            return 0
+
+        def sencode(msg, key):
+            l = len(msg)
+            pwd = []
+            for i in range(0, l, 4):
+                pwd.append(
+                    ordat(msg, i)
+                    | ordat(msg, i + 1) << 8
+                    | ordat(msg, i + 2) << 16
+                    | ordat(msg, i + 3) << 24
+                )
+            if key:
+                pwd.append(l)
+            return pwd
+
+        def lencode(msg, key):
+            l = len(msg)
+            ll = (l - 1) << 2
+            if key:
+                m = msg[l - 1]
+                if m < ll - 3 or m > ll:
+                    return
+                ll = m
+            for i in range(0, l):
+                msg[i] = (
+                        chr(msg[i] & 0xFF)
+                        + chr(msg[i] >> 8 & 0xFF)
+                        + chr(msg[i] >> 16 & 0xFF)
+                        + chr(msg[i] >> 24 & 0xFF)
+                )
+            if key:
+                return "".join(msg)[0:ll]
+            return "".join(msg)
+        pwd = sencode(msg, True)
+        pwdk = sencode(key, False)
+        if len(pwdk) < 4:
+            pwdk = pwdk + [0] * (4 - len(pwdk))
+        n = len(pwd) - 1
+        z = pwd[n]
+        y = pwd[0]
+        c = 0x86014019 | 0x183639A0
+        m = 0
+        e = 0
+        p = 0
+        q = floor(6 + 52 / (n + 1))
+        d = 0
+        while 0 < q:
+            d = d + c & (0x8CE0D9BF | 0x731F2640)
+            e = d >> 2 & 3
+            p = 0
+            while p < n:
+                y = pwd[p + 1]
+                m = z >> 5 ^ y << 2
+                m = m + ((y >> 3 ^ z << 4) ^ (d ^ y))
+                m = m + (pwdk[(p & 3) ^ e] ^ z)
+                pwd[p] = pwd[p] + m & (0xEFB8D130 | 0x10472ECF)
+                z = pwd[p]
+                p = p + 1
+            y = pwd[0]
+            m = z >> 5 ^ y << 2
+            m = m + ((y >> 3 ^ z << 4) ^ (d ^ y))
+            m = m + (pwdk[(p & 3) ^ e] ^ z)
+            pwd[n] = pwd[n] + m & (0xBB390742 | 0x44C6F8BD)
+            z = pwd[n]
+            q = q - 1
+        return lencode(pwd, False)
+
+    def __del__(self):
+        self.sess.close()
 
 
-def main():
+if __name__ == '__main__':
     # load parameters from command line
     parser = argparse.ArgumentParser(prog="bucealoginner")
-    parser.add_argument('--username', default=None)
-    parser.add_argument('--password', default=None)
-    parser.add_argument('--delaytime', default=7200)
-    parser.add_argument('--starttime', default=1)
-    parser.add_argument('--endtime', default=5)
-    parser.add_argument('--resttime', default=300)
+    parser.add_argument('--username',type=str, default='123')
+    parser.add_argument('--password',type=str, default='123')
     __args = parser.parse_args()
     # <<<
-
-    # >>> checking parameters
-    __user_name = __args.username
-    if isinstance(__user_name, str):
-        button = False
-        for _ in __user_name:
-            if _ not in [str(__) for __ in range(10)]: button = True
-        if button:
-            raise ValueError("Non-number included in parameter.[{}]".format("username"))
-    elif __user_name is None:
-        raise ValueError("Please input the important parameter .[{}]".format("username"))
-    elif isinstance(__user_name, int):
-        __user_name = str(__user_name)
-    else:
-        raise ValueError("Unknown parameter .[{}]".format("username"))
-
-    __pass_word = __args.password
-    if isinstance(__pass_word, str):
-        pass
-    elif isinstance(__pass_word, int):
-        __pass_word = str(__pass_word)
-    elif __pass_word is None:
-        raise ValueError("Please input the important parameter .[{}]".format("password"))
-    else:
-        raise ValueError("Unknown parameter .[{}]".format("password"))
-
-    __delay_time = __args.delaytime
-    if isinstance(__delay_time, str):
-        __delay_time = float(__delay_time)
-        __delay_time = int(__delay_time)
-    __start_time = __args.starttime
-    if isinstance(__start_time, str):
-        __start_time = float(__start_time)
-        __start_time = int(__start_time)
-    __end_time = __args.endtime
-    if isinstance(__end_time, str):
-        __end_time = float(__end_time)
-        __end_time = int(__end_time)
-    if __end_time < __start_time:
-        __tran = __start_time
-        __start_time = __end_time
-        __end_time = __tran
-    __rest_time = __args.resttime
-    if isinstance(__rest_time, str):
-        __rest_time = float(__rest_time)
-        __rest_time = int(__rest_time)
-    # <<<
-
-    # >>>  login loop
-    loginner = BuceaLoginner(user_name=__user_name,
-                             pass_word=__pass_word,
-                             start_time=__start_time,
-                             end_time=__end_time)
-    while True:
-        if loginner.down_time():
-            time.sleep(__rest_time)
-            continue
-        try:
-            loginner.bucea_login()
-        except Exception:
-            time.sleep(__rest_time)
-            loginner.bucea_login()
-        finally:
-            time.sleep(__delay_time)
-    # <<<
-
-
-if __name__ == "__main__":
-    main()
+    BuceaNet(__args.username,__args.password).login()
